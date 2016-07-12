b21c611e2559e6f875b7bfccfa9e403ca2df8e27
littemer <littemer@gmail.com>
2016-07-12 10:48:21 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,37 +6,38 @@ var srcDir = path.resolve('src');
  * @type {{entry: string[], output: {path: string, filename: string}, module: {loaders: *[]}}}
  */
 module.exports = {
-	entry: [
-		'webpack-dev-server/client?http://localhost:9090',
-		'webpack/hot/dev-server',
-		'./src/pages/app.js'
-	],
-	output: {
-		path: __dirname + '/build/',
-		publicPath: '/_kdf/',
-		filename: "bundle.js"
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.jsx$/,
-				exclude: /node_modules/,
-				//loader: "babel-loader!jsx-loader?harmony"
-				// 在这里添加 react-hot，注意这里使用的是loaders，所以不能用 query，应该把presets参数写在 babel 的后面
-				loaders: ['react-hot', 'babel?presets[]=react,presets[]=es2015']
-			}
-		]
-	},
-	plugins:[
-		new webpack.HotModuleReplacementPlugin()
-	],
-	resolve: {
-		root: path.resolve(__dirname),
-		alias:{
-			page: path.join(srcDir,'pages'),
-			comp: path.join(srcDir,'components'),
-			comp2: path.join(srcDir,'pages/components')
-		},
-		extensions:['','.js']
+entry: [
+	'webpack-dev-server/client?http://localhost:9090',
+	'webpack/hot/dev-server',
+	'./src/pages/app.js'
+],
+output: {
+	path: __dirname + '/build/',
+	publicPath: '/_kdf/',
+	filename: "bundle.js"
+},
+module: {
+	loaders: [
+	{
+	test: /\.jsx$/,
+	exclude: /node_modules/,
+	//loader: "babel-loader!jsx-loader?harmony"
+	// 在这里添加 react-hot，注意这里使用的是loaders，所以不能用 query，应该把presets参数写在 babel 的后面
+	loaders: ['react-hot', 'babel?presets[]=react,presets[]=es2015']
 	}
+	]
+},
+plugins:[
+	new webpack.HotModuleReplacementPlugin()
+],
+resolve: {
+	root: path.resolve(__dirname),
+	alias:{
+		page: path.join(srcDir,'pages'),
+		comp: path.join(srcDir,'components'),
+		comp2: path.join(srcDir,'pages/components')
+	},
+	extensions:['','.js']
+}
+	
 };
