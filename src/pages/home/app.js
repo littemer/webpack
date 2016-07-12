7b47bba83330338369e881dfc529d71fe936b3ac
ldw <littemer@gmail.com>
2016-07-13 00:02:48 +0800

    webpack react 搭建

--- src/pages/home/app.js
+++ src/pages/home/app.js
@@ -1,8 +1,9 @@
-// es 2016 语法
+// es 2016 语法////
 "use strict";
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HelloReact from './view/index';
+
 
 ReactDOM.render(<HelloReact />, document.getElementById('container'));
--- src/pages/home/view/index.js
+++ src/pages/home/view/index.js
@@ -4,6 +4,7 @@ class HelloReact extends React.Component {
 	// constructor(props){
 	// 	super(props);
 	// }
+	
 	// clickHandle(){
 	// 	alert('测试react');
 	// }
